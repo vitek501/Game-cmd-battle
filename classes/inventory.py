23be9dad8d483cdb9b9133f274cd580a39871ade
vitek501 <vitek.kurdyukov@gmail.com>
2021-02-14 11:54:34 +0500

    Init commit

---
+++ classes/inventory.py
@@ -0,0 +1,6 @@
+class Item:
+    def __init__(self, name, type, descriptions, prop):
+        self.name = name
+        self.type = type
+        self.descriptions = descriptions
+        self.prop = prop
